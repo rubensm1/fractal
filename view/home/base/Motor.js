8e1085975a9ead11914e29716bba194ab7f4912f
rubensm1 <rubensantoniomarcon@gmail.com>
2015-09-01 17:33:32 -0300

    Arrumando formatação

--- view/home/base/Motor.js
+++ view/home/base/Motor.js
@@ -1,117 +1,117 @@
 var Motor;
 
 Motor = (function () {
-	
-	function Motor (plano, planoRoot, caneta, pontoInicial, intervaloPulsos, fragmentos, cor, escala) {
-		
-		this.plano = plano;
-		this.planoRoot = planoRoot ? planoRoot : plano;
-		this.caneta = caneta;
-		this.ponto = pontoInicial;
-		
-		this.pulso = null;
-		this.intervaloPulsos = intervaloPulsos ? intervaloPulsos : INTERVALO_QUANT_PULSOS;
-		this.fragmentos = fragmentos ? fragmentos : FRAGMENTOS;
-		this.numerador = 0;
-		this.cor = cor ? cor : COR;
-		this.escala = escala ? escala : ESCALA;
-	}
-	
-	Motor.prototype.ligar = function (pulsante) {
-		if (this.isLigado() || !(pulsante instanceof Pulsante))
-			return;
-		
-		this.planoRoot.getSVG().appendChild(this.ponto.getSVG());
-		
-		var motor = this;
-		/*this.pulsoID = setInterval(function(){
-			motor.mover();
-			motor.desenhar();
-			motor.caneta.set(motor.ponto.x,motor.ponto.y);
-		}, this.intervaloPulsos);*/
-		var pulsoID = pulsante.novaAcao(function(){
-			motor.mover();
-			if (motor.ponto instanceof Ponto) {
-				motor.desenhar();
-				motor.caneta.set(motor.ponto.x,motor.ponto.y);
-			}
-		},this.intervaloPulsos);
-		this.pulso = {pulsoID: pulsoID, pulsante: pulsante};
-		return pulsoID;
-	}
-	
-	Motor.prototype.pausar = function () {
-		if (this.isLigado()) {
-			
-		}
-		else {
-			
-		}
-	}
-	
-	Motor.prototype.desligar = function () {
-		if (!this.isLigado())
-			return;
-		this.ponto.getSVG().remove();
-		//clearInterval(this.pulsoID);
-		this.pulso.pulsante.delAcao(this.pulso.pulsoID)
-		this.pulso = null;
-	}
-	
-	Motor.prototype.mover = function () {
-		var px, py;
-		px = this.escala * Math.sin(2 * Math.PI * this.numerador/this.fragmentos);
-		py = this.escala * Math.cos(2 * Math.PI * this.numerador/this.fragmentos);
-		this.numerador < this.fragmentos -1 ? this.numerador++ : this.numerador = 0;
-		//this.numerador > 0 ? this.numerador-- : this.numerador = this.fragmentos -1;
-		this.ponto.set(px,py);
-	}
-	
-	/*Motor.prototype.mover = function () {
-		var px, py;
-		px = this.numerador;
-		py = 0.01 *(px * px) - 300;
-		//py = -0.01 *(px * px) + 300;
-		//py = 0.00002*(px * px *px);
-		this.numerador < 220 ? this.numerador+=4 : this.numerador = -220;
-		this.ponto.set(px,py);
-	}*/
-	
-	Motor.prototype.desenhar = function () {
-		if (this.plano == this.planoRoot) {
-			var svgSegmento = new Segmento (this.caneta, this.ponto, this.cor).getSVG();
-			this.plano.getSVG().appendChild(svgSegmento);
-		}
-		else{
-			var transform = new Transform(this.plano);
-			transform.pontar(this.ponto);
-			var svgSegmento = new Segmento (this.caneta, this.ponto, this.cor).getSVG();
-			this.planoRoot.getSVG().appendChild(svgSegmento);
-			//transform.aplicar(svgSegmento, this.plano);
-			//transform.aplicar(svgSegmento, this.planoRoot.transform.baseVal.consolidate().matrix.inverse(),true);
-		}
-	}
-	
-	Motor.prototype.isLigado = function () {
-		return this.pulso != null;
-	}
-	
-	return Motor;
-	
+
+    function Motor(plano, planoRoot, caneta, pontoInicial, intervaloPulsos, fragmentos, cor, escala) {
+
+        this.plano = plano;
+        this.planoRoot = planoRoot ? planoRoot : plano;
+        this.caneta = caneta;
+        this.ponto = pontoInicial;
+
+        this.pulso = null;
+        this.intervaloPulsos = intervaloPulsos ? intervaloPulsos : INTERVALO_QUANT_PULSOS;
+        this.fragmentos = fragmentos ? fragmentos : FRAGMENTOS;
+        this.numerador = 0;
+        this.cor = cor ? cor : COR;
+        this.escala = escala ? escala : ESCALA;
+    }
+
+    Motor.prototype.ligar = function (pulsante) {
+        if (this.isLigado() || !(pulsante instanceof Pulsante))
+            return;
+
+        this.planoRoot.getSVG().appendChild(this.ponto.getSVG());
+
+        var motor = this;
+        /*this.pulsoID = setInterval(function(){
+         motor.mover();
+         motor.desenhar();
+         motor.caneta.set(motor.ponto.x,motor.ponto.y);
+         }, this.intervaloPulsos);*/
+        var pulsoID = pulsante.novaAcao(function () {
+            motor.mover();
+            if (motor.ponto instanceof Ponto) {
+                motor.desenhar();
+                motor.caneta.set(motor.ponto.x, motor.ponto.y);
+            }
+        }, this.intervaloPulsos);
+        this.pulso = {pulsoID: pulsoID, pulsante: pulsante};
+        return pulsoID;
+    }
+
+    Motor.prototype.pausar = function () {
+        if (this.isLigado()) {
+
+        }
+        else {
+
+        }
+    }
+
+    Motor.prototype.desligar = function () {
+        if (!this.isLigado())
+            return;
+        this.ponto.getSVG().remove();
+        //clearInterval(this.pulsoID);
+        this.pulso.pulsante.delAcao(this.pulso.pulsoID)
+        this.pulso = null;
+    }
+
+    Motor.prototype.mover = function () {
+        var px, py;
+        px = this.escala * Math.sin(2 * Math.PI * this.numerador / this.fragmentos);
+        py = this.escala * Math.cos(2 * Math.PI * this.numerador / this.fragmentos);
+        this.numerador < this.fragmentos - 1 ? this.numerador++ : this.numerador = 0;
+        //this.numerador > 0 ? this.numerador-- : this.numerador = this.fragmentos -1;
+        this.ponto.set(px, py);
+    }
+
+    /*Motor.prototype.mover = function () {
+     var px, py;
+     px = this.numerador;
+     py = 0.01 *(px * px) - 300;
+     //py = -0.01 *(px * px) + 300;
+     //py = 0.00002*(px * px *px);
+     this.numerador < 220 ? this.numerador+=4 : this.numerador = -220;
+     this.ponto.set(px,py);
+     }*/
+
+    Motor.prototype.desenhar = function () {
+        if (this.plano == this.planoRoot) {
+            var svgSegmento = new Segmento(this.caneta, this.ponto, this.cor).getSVG();
+            this.plano.getSVG().appendChild(svgSegmento);
+        }
+        else {
+            var transform = new Transform(this.plano);
+            transform.pontar(this.ponto);
+            var svgSegmento = new Segmento(this.caneta, this.ponto, this.cor).getSVG();
+            this.planoRoot.getSVG().appendChild(svgSegmento);
+            //transform.aplicar(svgSegmento, this.plano);
+            //transform.aplicar(svgSegmento, this.planoRoot.transform.baseVal.consolidate().matrix.inverse(),true);
+        }
+    }
+
+    Motor.prototype.isLigado = function () {
+        return this.pulso != null;
+    }
+
+    return Motor;
+
 })();
 
 /*
-var pulsante2 = new Pulsante(50);
-
-var planoG = new Plano (0,100);
-
-motores.push( new Motor(planoPrincipal,planoPrincipal,caneta,planoG) );
-motores[0].ligar(pulsante);
-
-motores.push( new Motor(planoG,planoPrincipal,caneta,new Ponto(0,150), INTERVALO_QUANT_PULSOS, FRAGMENTOS, COR, 150 ));
-motores.push( new Motor(planoG,planoPrincipal,caneta,new Ponto(0,150), INTERVALO_QUANT_PULSOS, FRAGMENTOS, COR, 150 ));
-
-motores[1].ligar(pulsante);
-motores[2].ligar(pulsante);
-iniciado = true;
-*/
+ var pulsante2 = new Pulsante(50);
+ 
+ var planoG = new Plano (0,100);
+ 
+ motores.push( new Motor(planoPrincipal,planoPrincipal,caneta,planoG) );
+ motores[0].ligar(pulsante);
+ 
+ motores.push( new Motor(planoG,planoPrincipal,caneta,new Ponto(0,150), INTERVALO_QUANT_PULSOS, FRAGMENTOS, COR, 150 ));
+ motores.push( new Motor(planoG,planoPrincipal,caneta,new Ponto(0,150), INTERVALO_QUANT_PULSOS, FRAGMENTOS, COR, 150 ));
+ 
+ motores[1].ligar(pulsante);
+ motores[2].ligar(pulsante);
+ iniciado = true;
+ */
--- view/home/base/Transform.js
+++ view/home/base/Transform.js
@@ -1,76 +1,76 @@
 var Transform;
 
 Transform = (function () {
-	
-	
-	function Transform (matrix) {
-		
-		if (matrix == null)
-			this.matrix = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
-		else if (matrix instanceof SVGMatrix)
-			this.matrix = matrix;
-		else if (matrix instanceof Plano)
-			this.matrix = matrix.getSVG().transform.baseVal.consolidate().matrix
-		else
-			this.matrix = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
-		
-		this.lastMatrix = null;
-	}
-	
-	Transform.MATRIX_IDENTIDADE = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
-	
-	Transform.prototype.set = function(matrix) {
-		if (matrix instanceof SVGMatrix) 
-			this.matrix = matrix;
-	}
-		
-	Transform.prototype.translate = function (x,y) {
-		this.lastMatrix = Transform.MATRIX_IDENTIDADE.translate(x,y);
-		return this.matrix.translate(x,y);
-	}
-	
-	Transform.prototype.rotate = function (g) {
-		this.lastMatrix = Transform.MATRIX_IDENTIDADE.rotate(g);
-		return this.matrix.rotate(g);
-	}
-	
-	Transform.prototype.scale = function (s) {
-		this.lastMatrix =  Transform.MATRIX_IDENTIDADE.scale(s);
-		return this.matrix.scale(s);
-	}
-	
-	Transform.prototype.aplicar = function (elemento,referencia,appendar) {
-		// matrix de transformação
-		var transformMatrix;
-		if (referencia == null) {
-			transformMatrix = this.matrix;
-		}
-		else if (referencia instanceof SVGMatrix) {
-			transformMatrix = referencia;
-		}
-		else if (referencia instanceof SVGElement) {
-			transformMatrix = referencia.getTransformToElement(elemento);
-		}
-		else 
-			return;
-		
-		// obter SVGTransform e carregar a matrix de transformação 
-		var svgTransform = elemento.transform.baseVal.createSVGTransformFromMatrix(transformMatrix);
-		
-		// aplicar transformação
-		if(appendar)
-			elemento.transform.baseVal.appendItem(svgTransform);
-		else
-			elemento.transform.baseVal.initialize(svgTransform);
-	}
-	
-	Transform.prototype.pontar = function (ponto) {
-		var x = ponto.x * this.matrix.a + ponto.x * this.matrix.c + this.matrix.e;
-		var y = ponto.y * this.matrix.b + ponto.y * this.matrix.d + this.matrix.f;
-		
-		ponto.set(x,y);
-	}
-	
-	return Transform;
-	
+
+
+    function Transform(matrix) {
+
+        if (matrix == null)
+            this.matrix = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
+        else if (matrix instanceof SVGMatrix)
+            this.matrix = matrix;
+        else if (matrix instanceof Plano)
+            this.matrix = matrix.getSVG().transform.baseVal.consolidate().matrix
+        else
+            this.matrix = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
+
+        this.lastMatrix = null;
+    }
+
+    Transform.MATRIX_IDENTIDADE = document.createElementNS("http://www.w3.org/2000/svg", 'circle').getTransformToElement(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
+
+    Transform.prototype.set = function (matrix) {
+        if (matrix instanceof SVGMatrix)
+            this.matrix = matrix;
+    }
+
+    Transform.prototype.translate = function (x, y) {
+        this.lastMatrix = Transform.MATRIX_IDENTIDADE.translate(x, y);
+        return this.matrix.translate(x, y);
+    }
+
+    Transform.prototype.rotate = function (g) {
+        this.lastMatrix = Transform.MATRIX_IDENTIDADE.rotate(g);
+        return this.matrix.rotate(g);
+    }
+
+    Transform.prototype.scale = function (s) {
+        this.lastMatrix = Transform.MATRIX_IDENTIDADE.scale(s);
+        return this.matrix.scale(s);
+    }
+
+    Transform.prototype.aplicar = function (elemento, referencia, appendar) {
+        // matrix de transformaï¿½ï¿½o
+        var transformMatrix;
+        if (referencia == null) {
+            transformMatrix = this.matrix;
+        }
+        else if (referencia instanceof SVGMatrix) {
+            transformMatrix = referencia;
+        }
+        else if (referencia instanceof SVGElement) {
+            transformMatrix = referencia.getTransformToElement(elemento);
+        }
+        else
+            return;
+
+        // obter SVGTransform e carregar a matrix de transformaï¿½ï¿½o 
+        var svgTransform = elemento.transform.baseVal.createSVGTransformFromMatrix(transformMatrix);
+
+        // aplicar transformaï¿½ï¿½o
+        if (appendar)
+            elemento.transform.baseVal.appendItem(svgTransform);
+        else
+            elemento.transform.baseVal.initialize(svgTransform);
+    }
+
+    Transform.prototype.pontar = function (ponto) {
+        var x = ponto.x * this.matrix.a + ponto.x * this.matrix.c + this.matrix.e;
+        var y = ponto.y * this.matrix.b + ponto.y * this.matrix.d + this.matrix.f;
+
+        ponto.set(x, y);
+    }
+
+    return Transform;
+
 })();
