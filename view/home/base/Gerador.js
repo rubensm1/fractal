67edd54e4c7dbd955fabeebdbaad54e4da7f5a42
rubensm1 <rubensantoniomarcon@gmail.com>
2025-07-04 00:19:04 -0400

    melhoria gerador

--- view/home/base/Gerador.js
+++ view/home/base/Gerador.js
@@ -19,7 +19,7 @@ Gerador = (function() {
     }
 
     Gerador.prototype.geraEscalas = function() {
-        return 250;
+        //return 250;
         return 1 + Math.floor(Math.random() * (ESCALA - 1));
     }
 
@@ -30,12 +30,18 @@ Gerador = (function() {
         var fragPonts = (20 + Math.floor(Math.random() * 80)) * 5;
         motores.novoMotor(new Motor(planoPrincipal, planoPrincipal, motores.getPincel(), planoG, INTERVALO_QUANT_PULSOS, fragPlan, COR, partEscala));
         motores.novoMotor(new Motor(planoG, planoPrincipal, motores.getPincel(), new Ponto(0, 150), INTERVALO_QUANT_PULSOS, fragPonts, COR, ESCALA - partEscala, 0.2, !SENTIDO), null, 0);
-        motores.novoMotor(new Motor(planoG, planoPrincipal, motores.getPincel(), new Ponto(0, 150), INTERVALO_QUANT_PULSOS, fragPonts, COR, ESCALA - partEscala, 0.2, !SENTIDO), null, 1);
+        motores.novoMotor(new Motor(planoG, planoPrincipal, motores.getPincel(), new Ponto(0, 150), INTERVALO_QUANT_PULSOS, fragPonts, COR, ESCALA - partEscala, 0.2, !SENTIDO), null, 10);
+        //motores.novoMotor(new Motor(planoG, planoPrincipal, motores.getPincel(), new Ponto(0, 150), INTERVALO_QUANT_PULSOS, fragPonts, COR, ESCALA - partEscala, 0.2, !SENTIDO), null, Math.floor(fragPonts / 2));
 
         var pulsoID = pulsante.novaAcao(function() {
             motores.desligarTudo();
             pulsante.delAcao(pulsoID);
         }, this.mmc(fragPlan, fragPonts));
+
+        console.log("Escalas: " + partEscala + ", " + (ESCALA - partEscala));
+        console.log("Fragmentos: " + fragPlan + ", " + fragPonts);
+        console.log("Ações total: " + this.mmc(fragPlan, fragPonts));
+
     }
 
     return Gerador;
